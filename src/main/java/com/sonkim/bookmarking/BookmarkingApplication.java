1df4a6085737eec491d311bf2c2536caffae0e92
son0307 <162094052+son0307@users.noreply.github.com>
2025-08-01 15:59:40 +0900

    스프링 기초 파일 생성

---
+++ src/main/java/com/sonkim/bookmarking/BookmarkingApplication.java
@@ -0,0 +1,13 @@
+package com.sonkim.bookmarking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkingApplication.class, args);
+	}
+
+}
