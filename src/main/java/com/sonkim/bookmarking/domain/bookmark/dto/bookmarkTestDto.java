ba08b4c050a677a2b32fe944d8f6c91204dabf5e
son0307 <162094052+son0307@users.noreply.github.com>
2025-08-02 17:54:49 +0900

    OpenGraph 정보 추출 테스트 완료

---
+++ src/main/java/com/sonkim/bookmarking/domain/bookmark/dto/bookmarkTestDto.java
@@ -0,0 +1,11 @@
+package com.sonkim.bookmarking.domain.bookmark.dto;
+
+import lombok.Data;
+
+@Data
+public class bookmarkTestDto {
+    private String title;
+    private String description;
+    private String url;
+    private String image;
+}
