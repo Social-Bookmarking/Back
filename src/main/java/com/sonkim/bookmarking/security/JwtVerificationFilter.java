a1673599d549e9adf6ade0916adaaa476776d598
son0307 <162094052+son0307@users.noreply.github.com>
2025-08-10 17:30:31 +0900

    초대 코드 기반 QR 코드 이미지 생성 기능 구현, 범용 예외 처리 메서드 추가

--- src/main/java/com/sonkim/bookmarking/security/JwtVerificationFilter.java
+++ src/main/java/com/sonkim/bookmarking/security/JwtVerificationFilter.java
@@ -42,37 +42,26 @@ public class JwtVerificationFilter extends OncePerRequestFilter {
         // 토큰 값 추출
         String token = authHeader.substring(7);
 
-        try {
-            // 카테고리 검증 추가
-            String category = jwtUtil.getCategoryFromJWT(token);
-            if (!"access".equals(category)) {
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token category");
-                return;
-            }
-
-            // JWT에서 사용자 정보 추출
-            Long userId = jwtUtil.getUserId(token);
-            String username = jwtUtil.getUsernameFromJWT(token);
-
-            // 인증 객체 생성
-            UserDetailsImpl userDetailsImpl = new UserDetailsImpl(userId, username);
-            Authentication auth = new UsernamePasswordAuthenticationToken(userDetailsImpl, null, userDetailsImpl.getAuthorities());
-
-            // SecurityContext에 인증 정보 저장
-            if (SecurityContextHolder.getContext().getAuthentication() == null) {
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            }
-        } catch (io.jsonwebtoken.ExpiredJwtException e) {
-            log.error("만료된 Access Token {}", e.getMessage());
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "만료된 Access Token 입니다.");
-            return;
-        }
-        catch (Exception e) {
-            log.error("JWT 필터 처리 중 에러 발생 : {}", e.getMessage());
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않은 토큰입니다.");
+        // 카테고리 검증 추가
+        String category = jwtUtil.getCategoryFromJWT(token);
+        if (!"access".equals(category)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid token category");
             return;
         }
 
-        filterChain.doFilter(request, response);
+        // JWT에서 사용자 정보 추출
+        Long userId = jwtUtil.getUserId(token);
+        String username = jwtUtil.getUsernameFromJWT(token);
+
+        // 인증 객체 생성
+        UserDetailsImpl userDetailsImpl = new UserDetailsImpl(userId, username);
+        Authentication auth = new UsernamePasswordAuthenticationToken(userDetailsImpl, null, userDetailsImpl.getAuthorities());
+
+        // SecurityContext에 인증 정보 저장
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            filterChain.doFilter(request, response);
+        }
     }
 }
