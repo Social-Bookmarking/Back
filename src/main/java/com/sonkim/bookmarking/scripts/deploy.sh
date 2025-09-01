b4d308a032ecd126dd3618b4d29a5f107c5a64ec
son0307 <162094052+son0307@users.noreply.github.com>
2025-09-01 14:59:03 +0900

    CI/CD 시스템 구축

---
+++ src/main/java/com/sonkim/bookmarking/scripts/deploy.sh
@@ -0,0 +1,32 @@
+REPOSITORY=/home/ec2-user/app
+cd $REPOSITORY
+
+APP_NAME=MarkSphere
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+PORT=8080
+CURRENT_PID=$(lsof -t -i:$PORT)
+
+
+if [ -z "$CURRENT_PID" ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> 정상 종료 시도: kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
+  sleep 3
+
+  if kill -0 $CURRENT_PID > /dev/null 2>&1
+  then
+    echo "> 프로세스가 종료되지 않아 강제 종료합니다: kill -9 $CURRENT_PID"
+    sudo kill -9 $CURRENT_PID
+  else
+    echo "> 정상적으로 종료되었습니다."
+  fi
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar \
+    -Dspring.profiles.active=dev \
+    $JAR_PATH > /home/ubuntu/nohup.out 2>&1 &
